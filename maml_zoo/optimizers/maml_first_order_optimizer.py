f121f19a87a877b4e60eee77becb8e52d43e10e9
Ignasi <ignasi78@gmail.com>
2018-08-15 13:39:09 -0700

    documentation

--- maml_zoo/optimizers/maml_first_order_optimizer.py
+++ maml_zoo/optimizers/maml_first_order_optimizer.py
@@ -7,7 +7,19 @@ class MAMLFirstOrderOptimizer(Optimizer):
     """
     Optimizer for first order methods (SGD, Adam)
 
+    Args:
+        tf_optimizer_cls (tf.train.optimizer): desired tensorflow optimzier for training
+        tf_optimizer_args (dict or None): arguments for the optimizer
+        learning_rate (float): learning rate
+        max_epochs: number of maximum epochs for training
+        tolerance (float): tolerance for early stopping. If the loss fucntion decreases less than the specified tolerance
+        after an epoch, then the training stops.
+        num_minibatches (int): number of mini-batches for performing the gradient step. The mini-batch size is
+        batch size//num_minibatches.
+        verbose (bool): Whether to log or not the optimization process
+
     """
+
     def __init__(
             self,
             tf_optimizer_cls=tf.train.AdamOptimizer,
@@ -18,19 +30,7 @@ class MAMLFirstOrderOptimizer(Optimizer):
             num_minibatches=1,
             verbose=False
             ):
-        """
 
-        Args:
-            tf_optimizer_cls (tf.train.optimizer): desired tensorflow optimzier for training
-            tf_optimizer_args (dict or None): arguments for the optimizer
-            learning_rate (float): learning rate
-            max_epochs: number of maximum epochs for training
-            tolerance (float): tolerance for early stopping. If the loss fucntion decreases less than the specified tolerance
-            after an epoch, then the training stops.
-            num_minibatches (int): number of mini-batches for performing the gradient step. The mini-batch size is
-            batch size//num_minibatches.
-            verbose (bool): Whether to log or not the optimization process
-        """
         self._target = None
         if tf_optimizer_args is None:
             tf_optimizer_args = dict()
@@ -45,7 +45,7 @@ class MAMLFirstOrderOptimizer(Optimizer):
         self._train_op = None
         self._loss = None
         
-    def build_graph(self, loss, target, inputs, extra_inputs=(), **kwargs):
+    def build_graph(self, loss, target, inputs, extra_inputs=[], **kwargs):
         # TODO: Can we get rid of the extra_inputs?? And just have them for the auxiliary objectives
         # and not for the main loss
         """
@@ -54,11 +54,11 @@ class MAMLFirstOrderOptimizer(Optimizer):
         Args:
             loss (tf_op) : minimization objective
             target (Policy) : Policy whose values we are optimizing over
-            inputs (tuple) : tuple of tf.placeholders for input data 
-            extra_inputs (tuple) : tuple of tf.placeholders for hyperparameters (e.g. learning rate, if annealed)
+            inputs (list) : list of tf.placeholders for input data
+            extra_inputs (list) : list of tf.placeholders for hyperparameters (e.g. learning rate, if annealed)
         """
-        assert isinstance(inputs, tuple)
-        assert isinstance(extra_inputs, tuple)
+        assert isinstance(inputs, list)
+        assert isinstance(extra_inputs, list)
 
         self._target = target
         self._train_op = self._tf_optimizer.minimize(loss, var_list=target.get_params())
@@ -70,8 +70,8 @@ class MAMLFirstOrderOptimizer(Optimizer):
         Computes the value of the loss for given inputs
 
         Args:
-            inputs (tuple): inputs needed to compute the loss function
-            extra_inputs (tuple): additional inputs needed to compute the loss function
+            inputs (list): inputs needed to compute the loss function
+            extra_inputs (list): additional inputs needed to compute the loss function
 
         Returns:
             (float): value of the loss
@@ -89,8 +89,8 @@ class MAMLFirstOrderOptimizer(Optimizer):
         Carries out the optimization step
 
         Args:
-            inputs (tuple): inputs for the optimization
-            extra_inputs (tuple): extra inputs for the optimization
+            inputs (list): inputs for the optimization
+            extra_inputs (list): extra inputs for the optimization
 
         """
         assert isinstance(inputs, list)
@@ -107,7 +107,7 @@ class MAMLFirstOrderOptimizer(Optimizer):
 
         for epoch in range(self._max_epochs):
             if self._verbose:
-                logger.log("Epoch %d" % (epoch))
+                logger.log("Epoch %d" % epoch)
 
             sess.run(self._train_op, dict(list(zip(self._all_inputs, all_inputs))))
                 
@@ -132,17 +132,17 @@ class MAMLPPOOptimizer(MAMLFirstOrderOptimizer):
         self._inner_kl = None
         self._outer_kl = None
 
-    def build_graph(self, loss, target, inputs, inner_kl=None, outer_kl=None, extra_inputs=(), **kwargs):
+    def build_graph(self, loss, target, inputs, inner_kl=None, outer_kl=None, extra_inputs=[], **kwargs):
         """
         Sets the objective function and target weights for the optimize function
 
         Args:
             loss (tf_op) : minimization objective
             target (Policy) : Policy whose values we are optimizing over
-            inputs (tuple) : tuple of tf.placeholders for input data
-            inner_kl (list): List with the inner kl loss for each task
-            outer_kl (list): List with the outer kl loss for each task
-            extra_inputs (tuple) : tuple of tf.placeholders for hyperparameters (e.g. learning rate, if annealed)
+            inputs (lsit) : list of tf.placeholders for input data
+            inner_kl (list): list with the inner kl loss for each task
+            outer_kl (list): list with the outer kl loss for each task
+            extra_inputs (list) : list of tf.placeholders for hyperparameters (e.g. learning rate, if annealed)
         """
         super(MAMLPPOOptimizer, self).build_graph(loss, target, inputs, extra_inputs)
         assert inner_kl is not None
@@ -155,8 +155,8 @@ class MAMLPPOOptimizer(MAMLFirstOrderOptimizer):
         Computes the value of the KL-divergence between pre-update policies for given inputs
 
         Args:
-            inputs (tuple): inputs needed to compute the inner KL
-            extra_inputs (tuple): additional inputs needed to compute the inner KL
+            inputs (list): inputs needed to compute the inner KL
+            extra_inputs (list): additional inputs needed to compute the inner KL
 
         Returns:
             (float): value of the loss
@@ -173,8 +173,8 @@ class MAMLPPOOptimizer(MAMLFirstOrderOptimizer):
         Computes the value of the KL-divergence between post-update policies for given inputs
 
         Args:
-            inputs (tuple): inputs needed to compute the outer KL
-            extra_inputs (tuple): additional inputs needed to compute the outer KL
+            inputs (list): inputs needed to compute the outer KL
+            extra_inputs (list): additional inputs needed to compute the outer KL
 
         Returns:
             (float): value of the loss
