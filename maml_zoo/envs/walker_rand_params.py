81c6477cd4ac8b2c082c274c8483c865da46a2a7
Ignasi <ignasi78@gmail.com>
2018-08-20 13:37:12 -0700

    rand_param envs

--- maml_zoo/envs/walker_rand_params.py
+++ maml_zoo/envs/walker_rand_params.py
@@ -4,29 +4,27 @@ from gym import utils
 from maml_zoo.logger import logger
 
 
-class HopperRandParamsEnv(RandomEnv, utils.EzPickle):
+class Walker2DRandParamsEnv(RandomEnv, utils.EzPickle):
     def __init__(self, log_scale_limit=3.0):
-        RandomEnv.__init__(self, log_scale_limit, 'hopper.xml', 4)
+        RandomEnv.__init__(self, log_scale_limit, 'walker2d.xml', 5)
         utils.EzPickle.__init__(self)
 
     def step(self, a):
         posbefore = self.sim.data.qpos[0]
         self.do_simulation(a, self.frame_skip)
-        posafter, height, ang = self.sim.data.qpos[0:3]
+        posafter, height, ang = self.model.data.qpos[0:3]
         alive_bonus = 1.0
         reward = (posafter - posbefore) / self.dt
         reward += alive_bonus
         reward -= 1e-3 * np.square(a).sum()
-        s = self.state_vector()
-        done = not (np.isfinite(s).all() and (np.abs(s[2:]) < 100).all() and
-                    (height > .7) and (abs(ang) < .2))
+        done = not (2.0 > height > 0.8 and 1.0 > ang > -1.0)
         ob = self._get_obs()
-        return ob, reward, done, {}
+        return ob, float(reward), done, {}
 
     def _get_obs(self):
         return np.concatenate([
-            self.sim.data.qpos.flat[1:],
-            np.clip(self.sim.data.qvel.flat, -10, 10)
+            self.sim.data.qpos[1:].flat,
+            np.clip(self.model.data.qvel.flat, -10, 10)
         ])
 
     def reset_model(self):
@@ -48,7 +46,7 @@ class HopperRandParamsEnv(RandomEnv, utils.EzPickle):
 
 if __name__ == "__main__":
 
-    env = HopperRandParamsEnv()
+    env = Walker2DRandParamsEnv()
     tasks = env.sample_tasks(40)
     while True:
         env.reset()
