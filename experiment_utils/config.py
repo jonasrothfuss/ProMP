205adea8cba67681b4cd8d16f2991eba76aa2eb4
jonasrothfuss <jonas.rothfuss@gmx.de>
2018-08-23 11:37:44 -0700

    cleanup environment.yml and dockerfile

--- experiment_utils/config.py
+++ experiment_utils/config.py
@@ -4,6 +4,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 
 DOCKER_MOUNT_DIR = '/root/code/data'
 
-DOCKER_IMAGE = 'dennisl88/maml_zoo'
+DOCKER_IMAGE = 'jonasrothfuss/maml-zoo'
 
 S3_BUCKET_NAME = 'maml-zoo-experiments'
