7cefd8cd67335fa91c083faf02a2ef51da71a88c
jonasrothfuss <jonas.rothfuss@gmx.de>
2018-08-24 11:08:11 -0700

    fix integration test

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,6 +1,5 @@
 from maml_zoo.baselines.linear_feature_baseline import LinearFeatureBaseline
-from maml_zoo.envs.point_env_2d import MetaPointEnv
-from maml_zoo.meta_algos.ppo_maml import MAMLPPO
+from maml_zoo.meta_algos.ppo_maml import PPOMAML
 from maml_zoo.meta_trainer import Trainer
 from maml_zoo.samplers.maml_sampler import MAMLSampler
 from maml_zoo.samplers.maml_sample_processor import MAMLSampleProcessor
@@ -10,6 +9,72 @@ from maml_zoo.logger import logger
 import tensorflow as tf
 import numpy as np
 import unittest
+
+from gym.spaces import Box
+
+
+class MetaPointEnv():
+
+    def step(self, action):
+        """
+        Run one timestep of the environment's dynamics. When end of episode
+        is reached, reset() should be called to reset the environment's internal state.
+
+        Args:
+            action : an action provided by the environment
+        Returns:
+            (observation, reward, done, info)
+            observation : agent's observation of the current environment
+            reward [Float] : amount of reward due to the previous action
+            done : a boolean, indicating whether the episode has ended
+            info : a dictionary containing other diagnostic information from the previous action
+        """
+        prev_state = self._state
+        self._state = prev_state + np.clip(action, -0.1, 0.1)
+        reward = self.reward(prev_state, action, self._state)
+        done = self.done(self._state)
+        next_observation = np.copy(self._state)
+        return next_observation, reward, done, {}
+
+    def reset(self):
+        """
+        Resets the state of the environment, returning an initial observation.
+        Outputs
+        -------
+        observation : the initial observation of the space. (Initial reward is assumed to be 0.)
+        """
+        self._state = np.random.uniform(-2, 2, size=(2,))
+        observation = np.copy(self._state)
+        return observation
+
+    @property
+    def observation_space(self):
+        return Box(low=-np.inf, high=np.inf, shape=(2,))
+
+    @property
+    def action_space(self):
+        return Box(low=-0.1, high=0.1, shape=(2,))
+
+    def done(self, obs):
+        if obs.ndim == 1:
+            return abs(obs[0]) < 0.01 and abs(obs[1]) < 0.01
+        elif obs.ndim == 2:
+            return np.logical_and(np.abs(obs[:, 0]) < 0.01, np.abs(obs[:, 1]) < 0.01)
+
+    def reward(self, obs, act, obs_next):
+        if obs_next.ndim == 1:
+            return - np.sqrt(obs_next[0]**2 + obs_next[1]**2)
+        elif obs_next.ndim == 2:
+            return - np.sqrt(obs_next[:, 0] ** 2 + obs_next[:, 1] ** 2)
+
+    def log_diagnostics(self, paths):
+        pass
+
+    def sample_tasks(self, n_tasks):
+        return [{}] * n_tasks
+
+    def set_task(self, task):
+        pass
 
 class TestLikelihoodRation(unittest.TestCase):
     """
@@ -41,7 +106,7 @@ class TestLikelihoodRation(unittest.TestCase):
             parallel=False,
         )
 
-        self.sample_processor = sample_processor = MAMLSampleProcessor(
+        self.sample_processor = MAMLSampleProcessor(
             baseline=baseline,
             discount=0.99,
             gae_lambda=1.0,
@@ -49,13 +114,13 @@ class TestLikelihoodRation(unittest.TestCase):
             positive_adv=False,
         )
 
-        self.algo = MAMLPPO(
+        self.algo = PPOMAML(
             policy=policy,
             inner_lr=0.1,
             meta_batch_size=10,
             num_inner_grad_steps=2,
             learning_rate=1e-3,
-            max_epochs=300,
+            num_ppo_steps=5,
             num_minibatches=1,
             clip_eps=0.5,
             clip_outer=True,
@@ -66,7 +131,6 @@ class TestLikelihoodRation(unittest.TestCase):
             adaptive_outer_kl_penalty=False,
             adaptive_inner_kl_penalty=True,
             anneal_factor=1.0,
-            entropy_bonus=0.0,
         )
 
     def test_likelihood_ratio(self):
